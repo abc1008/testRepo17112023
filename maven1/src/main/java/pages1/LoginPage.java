6ee502401dfbe9980ed47a95dd013f1d68884e8c
Aditya <ag89111@gmail.com>
2023-11-17 19:01:39 +0530

    First Push Test

--- maven1/src/main/java/pages1/LoginPage.java
+++ maven1/src/main/java/pages1/LoginPage.java
@@ -68,6 +68,7 @@ public class LoginPage {
 			{
 				Reporter.log("User logged out successfully.", true);
 				Reporter.log("Test note for GIT.", true);
+				Reporter.log("Test note for GIT2.", true);
 			}
 		}
 	
